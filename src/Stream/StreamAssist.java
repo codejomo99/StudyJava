665b5e2e5e8fbec49ba9c4a4fd30b2d398a9fb25
Jong-Min <codejomo99@gmail.com>
2024-01-30 17:38:18 +0900

    Cutomer 객체 생성

---
+++ src/Stream/StreamAssist.java
@@ -0,0 +1,18 @@
+package Stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamAssist {
+    public static void main(String[] args){
+        List<Customer> cutomers = new ArrayList<>();
+        cutomers.add(new Customer("Song",45));
+        cutomers.add(new Customer("Kim",33));
+        cutomers.add(new Customer("Park",21));
+        cutomers.add(new Customer("Lee",67));
+        cutomers.add(new Customer("Choi",19));
+
+
+
+    }
+}
